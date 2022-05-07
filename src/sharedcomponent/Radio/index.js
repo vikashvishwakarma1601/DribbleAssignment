6caa9c320d0ac5ff0b6ab48891c4a9d995c4cf6e
AzureAD\VikashVishwakarma <vikash.v@mantralabsglobal.com>
2022-05-08 03:39:18 +0530

    Fixed Height Issue of Radio Box

--- src/sharedcomponent/Radio/index.js
+++ src/sharedcomponent/Radio/index.js
@@ -11,7 +11,7 @@ export default function (props) {
   return (
     <div className={styles.form_group}>
       <input id={props.title} type="radio" name={props.name} onChange={handleRadio} value={value}></input>
-      <label htmlFor={props.title}>
+      <label htmlFor={props.title} className={styles.label}>
         {props.icon && <i className={props.icon} aria-hidden="true"></i>}
         <div className={styles.textContent}>
           <h3 className={styles.title}>{props.title}</h3>
